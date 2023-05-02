24403ff06958255d3f0771e9e18506584fc0721a
williamfernandessilva <willgsvs@hotmail.com>
2023-05-02 16:51:43 -0300

    final

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {Client} from './client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  
+ constructor (private http: HttpClient){}
+ getClients(): Observable<Client[]>{
+  let url = "http://localhost:3000/clients";
+  return this.http.get<Client[]>(url)
+ }
+
+
 }
