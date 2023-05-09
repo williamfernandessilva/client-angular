ef14b8c0014b8d9d321cc0553b820dc174b4b8a5
williamfernandessilva <willgsvs@hotmail.com>
2023-05-09 16:54:32 -0300

    finall

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit,  } from '@angular/core';
 import { ClientService } from '../client.service';
 import { Client } from '../client';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-clients',
@@ -10,8 +12,19 @@ import { Client } from '../client';
 export class ClientsComponent implements OnInit {
 
   clients: Client[] = [];
+  isEditing : Boolean = false;
+  formGroupClient : FormGroup;
 
-  constructor(private clientService: ClientService){}
+
+  constructor(private clientService: ClientService, 
+              private formBuilder: FormBuilder) {
+    this.formGroupClient = formBuilder.group({
+      id : [''],
+      name : [''],
+      email : ['']
+
+    });
+  }
 
   ngOnInit(): void {
     this.loadClients();
@@ -22,12 +35,64 @@ export class ClientsComponent implements OnInit {
       {
         next : data => this.clients = data
       }
+    );
+  
+  }
 
+ save(){
+if(this.isEditing)
 
+    {
+      this.clientService.update(this.formGroupClient.value).subscribe(
+        {
+          next: data => {
+            this.loadClients();
+            this.formGroupClient.reset();
+            this.isEditing = false;
+
+          }
+        }
+      )
+
+    }
+    else{
+    this.clientService.save(this.formGroupClient.value).subscribe(
+    {
+        next: data => {
+          this.clients.push(data);
+          this.formGroupClient.reset();
+        }
+    }
 
     );
-
-   
   }
+
+
+      
+  }
+
+  clean(){
+    this.formGroupClient.reset();
+    this.isEditing = false;
+  }
+
+  edit(client: Client){
+    this.formGroupClient.setValue(client);
+    this.isEditing = true;
+    
+
+
+
+  }
+
+  delete(client: Client){
+    this.clientService.delete(client).subscribe({
+      next: ()=> this.loadClients()
+    })
+
+
+  }
+
+
 
 }
